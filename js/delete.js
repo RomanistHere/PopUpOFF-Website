5d2febb2c5e522787913916d14299af7085e23b5
romanisthere <romanisthere@gmail.com>
2020-12-28 21:12:16 +0300

    add browser detection

--- js/delete.js
+++ js/delete.js
@@ -3,6 +3,21 @@ const inputs = document.querySelectorAll('.form__inp')
 const textArea = document.querySelector('.form__area')
 const popup = document.querySelector('.popup_overlay')
 
+const browser = (function() {
+    const test = function(regexp) {return regexp.test(window.navigator.userAgent)}
+    switch (true) {
+        case test(/edg/i): return "Microsoft Edge";
+        case test(/trident/i): return "Microsoft Internet Explorer";
+        case test(/firefox|fxios/i): return "Mozilla Firefox";
+        case test(/opr\//i): return "Opera";
+        case test(/ucbrowser/i): return "UC Browser";
+        case test(/samsungbrowser/i): return "Samsung Browser";
+        case test(/chrome|chromium|crios/i): return "Google Chrome";
+        case test(/safari/i): return "Apple Safari";
+        default: return "Other";
+    }
+})();
+
 const handleErrors = (response) => {
     if (!response.ok) {
         throw Error(response.statusText)
@@ -22,7 +37,7 @@ form.addEventListener('submit', e => {
 
     const send = {
         inputs: checked,
-        comment: textArea.value
+        comment: browser + ' || ' + textArea.value
     }
 
     fetch('https://romanisthere.com:3000/quiz', {
